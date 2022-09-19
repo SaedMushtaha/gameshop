b695cea76b9717b8fba63dea3bbb4152bddba05f
S.A.E.D <S.A.E.D@DESKTOP-7H26SAC>
2022-09-19 13:58:11 +0300

    Implement API request caching

--- src/rawg-api/api.ts
+++ src/rawg-api/api.ts
@@ -3,20 +3,26 @@ const api = {
     key: 'eceff5221ab04eeba3e9f2ccb4124059',
   };
   
-  interface ResponseSchema<T> {
-    count: number,
-    next: string,
-    previous: string,
-    results: T[],
-  }
-  
-  async function get<T>(endpoint: string, params?: Record<string, string>): Promise<T> {
-    const searchParams = new URLSearchParams(params);
-    const response = await fetch(`${api.url}${endpoint}?${searchParams}&key=${api.key}`);
-    if (!response.ok) throw new Error(response.statusText);
-    const data = await response.json();
-    return data;
-  }
-  
-  export type { ResponseSchema };
-  export { get };
+  const cachedRequests: Record<string, unknown> =
+  JSON.parse(localStorage.getItem('cachedRequests') || '{}');
+
+interface ResponseSchema<T> {
+  count: number,
+  next: string,
+  previous: string,
+  results: T[],
+}
+
+async function get<T>(endpoint: string, params?: Record<string, string>): Promise<T> {
+  const searchParams = new URLSearchParams(params).toString();
+  if (cachedRequests[searchParams]) return cachedRequests[searchParams] as Promise<T>;
+  const response = await fetch(`${api.url}${endpoint}?${searchParams}&key=${api.key}`);
+  if (!response.ok) throw new Error(response.statusText);
+  const data = await response.json();
+  cachedRequests[searchParams] = data;
+  localStorage.setItem('cachedRequests', JSON.stringify(cachedRequests));
+  return data;
+}
+
+export type { ResponseSchema };
+export { get };
